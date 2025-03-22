902ed961c9c1b71d63269039370ebcb834807b65
Fahrizal231 <acioospprt@gmail.com>
2025-03-22 14:46:44 +0700

    Create khodam.js

---
+++ src/api/random/khodam.js
@@ -0,0 +1,28 @@
+const axios = require('axios');
+
+module.exports = function(app) {
+    async function fetchKhodam(name) {
+        try {
+            const response = await axios.get(`https://khodam.vercel.app/v2?nama=${encodeURIComponent(name)}`);
+            return response.data;
+        } catch (error) {
+            console.error("Error fetching Khodam data:", error);
+            throw error;
+        }
+    }
+
+    app.get('/random/khodam', async (req, res) => {
+        try {
+            const { name } = req.query;
+            if (!name) {
+                return res.status(400).json({ status: false, error: "Parameter 'name' wajib diisi" });
+            }
+
+            const result = await fetchKhodam(name);
+            res.status(200).json({ status: true, result });
+
+        } catch (error) {
+            res.status(500).json({ status: false, error: error.message });
+        }
+    });
+};
