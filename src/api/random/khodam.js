6288d74af647b78bf35b084f7f69b06688c1b9cc
Fahrizal231 <acioospprt@gmail.com>
2025-03-22 14:50:05 +0700

    Update khodam.js

--- src/api/random/khodam.js
+++ src/api/random/khodam.js
@@ -11,7 +11,7 @@ module.exports = function(app) {
         }
     }
 
-    app.get('/random/khodam', async (req, res) => {
+    app.get('memek', async (req, res) => {
         try {
             const { name } = req.query;
             if (!name) {
